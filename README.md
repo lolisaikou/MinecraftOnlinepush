7c15d4f90439300230eef5159615f4c98673b0ec
lolisaikou <32028165+lolisaikou@users.noreply.github.com>
2020-05-09 13:44:30 +0800

    V1.0

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # MinecraftOnlinepush
- 指定玩家上线微信推送 WeChatPush
+**通过微信推送玩家上线提醒的python脚本~~方便狙击(bushi)~~**  
+
+这个程序使用了C#编写的[MinecraftOutClient](https://github.com/Nsiso/MinecraftOutClient)作为检测在线玩家的部分，我修改了TestClient的部分代码，让python可以和TestClient交互（方便自定义服务器地址）  
+不重复造轮子(X)~~其实是不会C#~~，也看不懂[Protocol文档](http://wiki.vg/Protocol)
+
+***
+
+> 直接使用的方法  
+> 1. 到[这里](https://github.com/lolisaikou/MinecraftOnlinepush/releases/)下载我编译好的程序  
+> 2. 申请微信推送需要的SCKEY，在[这里](http://sc.ftqq.com/)申请  
+> 3. 用你喜欢的编辑器打开main.py把SCKEY、需要监控的服务器地址、端口、玩家id填入相应位置
+> 4. 运行main.py就行了  
+
+***
+
+> 自行编译  
+> 1. 编译TestClient  
+> 2. 然后按照把main.py放在TestClient.exe放在一起  
+> 3. 打开main.py把SCKEY、需要监控的服务器地址、端口、玩家id填入相应位置  
+> 4. 运行main.py  
+***
+因为玩家检测的部分是C#写的，所以不能再放在linux服务器上运行，希望有大佬可以用可移植的语言写一个出来  
+最后求一个star~
---
+++ TestClient/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using MinecraftOutClient.Modules;
+using System.Net.Sockets;
+using System.IO;
+using System.Linq;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {   
+                StreamWriter sw = new StreamWriter(@".\Output.txt");
+                Console.SetOut(sw);
+                Console.Clear();
+				//读取python脚本生成的服务器地址
+                string ip = File.ReadLines(@".\service.txt").Skip(0).Take(1).First(); // 读取第一行的服务器地址
+                string portstring = File.ReadLines(@".\service.txt").Skip(1).Take(1).First(); // 读取第二行的端口
+				// 固化到程序内
+                // string ip = ""; // 这里设定服务器地址
+                // string portstring = ""; //设定服务器端口
+                int port;
+                port = int.Parse(portstring);
+                try
+                {
+                    ServerInfo info = new ServerInfo(ip, port);
+                    info.StartGetServerInfo();
+                    foreach (string item in info.OnlinePlayersName)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+                catch (SocketException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.Write("连接发生异常。");
+                    continue;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    Console.Write("发生异常。");
+                    continue;
+                }
+                sw.Flush();
+                sw.Close();
+                break;
+            }
+        }
+    }
+}
